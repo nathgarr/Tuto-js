7ffef8478291d021312044ef39f5fe5e3154686b
nathgarr <nathgarr@outlook.fr>
2021-10-08 15:16:35 +0200

    feat:brulant et chaux

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const coeurPlein ='<ion-icon name="heart"></ion-icon>';
 //Variable couleur
 const bgFroid ='linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)';
 const bgTiède ='linear-gradient(120deg, #f6d365 0%, #fda085 100%)';
-const bgChaux='linear-gradient(to top, #f43b47 0%, #453a94 100%)';
-const bgBrulant='linear-gradient(to right, #ff8177 0%, #b12a5b 100%)';
+const bgChaux='linear-gradient(to right, #ff8177 0%, #b12a5b 100%)';
+const bgBrulant='linear-gradient(to top, #f43b47 0%, #453a94 100%)';
 
 const bgWin="linear-gradient(-225deg,#231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%)";
 const bgloose='linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)'
